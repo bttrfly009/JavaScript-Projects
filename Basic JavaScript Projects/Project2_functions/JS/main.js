ce0d0dcbf77c2c18fd44b604b2e57c4d31b2a99d
bttrfly009 <bttrfly009@gmail.com>
2022-01-05 09:29:47 -0500

    Add files via upload

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var sentence = "Roses are red,";
-    sentence += " and violets are blue.";
+    var sentence = "Roses are red,"; //Sets first variable
+    sentence += " and violets are blue."; //Adds to it
     document.getElementById("Concatenate").innerHTML = sentence;
 }
