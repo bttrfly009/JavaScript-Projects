3fd46d0a5bac25e331d48948b93a663783fb0290
bttrfly009 <bttrfly009@gmail.com>
2022-01-08 11:53:25 -0500

    Delete Basic JavaScript Projects/Project5_comparisons_type_coercion directory

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++
@@ -1,6 +0,0 @@
-function Ride_Function() {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
-}
