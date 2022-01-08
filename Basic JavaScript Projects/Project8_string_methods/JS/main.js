addc2869164474cba068ca0b583057e2fa2948d6
bttrfly009 <bttrfly009@gmail.com>
2022-01-08 18:57:33 -0500

    Slice/String/Precision/Concatenate

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -11,3 +11,11 @@ function precision_Method() {
     var Y = 12938.3012987376112;
     document.getElementById("Precision").innerHTML = Y.toPrecision(10);
 }
+function full_Sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
